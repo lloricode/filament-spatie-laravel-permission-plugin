df1118fdf13528d7aecfc4b1d97a00be03bce602
Lloric Mayuga Garcia <lloricode@gmail.com>
2024-11-11 07:47:29 +0800

    wip
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -3,5 +3,18 @@
 declare(strict_types=1);
 
 it('will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'rd', 'die', 'eval', 'sleep'])
     ->each->not->toBeUsed();
+
+arch()
+    ->preset()
+    ->laravel();
+
+arch()
+    ->preset()
+    ->php()
+    ->ignoring(['debug_backtrace']);
+
+arch()
+    ->preset()
+    ->security();
